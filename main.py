014a5a2678db4e89d044f3fe1dac82ee8452bc6d
diegotux <zaphifree@gmail.com>
2025-07-05 11:09:01 -0300

    in process of exercise "Print a Report"

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import count_words
+from stats import count_characters
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,5 +8,13 @@ def get_book_text(filepath):
         return book_content
     
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     count_words(get_book_text("books/frankenstein.txt"))
+    print("--------- Character Count -------")
+    temp_var = count_characters(get_book_text("books/frankenstein.txt"))
+    #temp_var.sort()
+    # print(type(temp_var))
+    print(temp_var)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def count_words(book):
     words = len(book.split())
     print(f"{words} words found in the document")
+
+def count_characters(text):
+    text = text.lower()
+    count_chars = {}
+    for char in text:
+        if char in count_chars:
+            count_chars[char] += 1
+        else:
+            count_chars[char] = 1 
+    
+
+def sort_items(count_chars):
+    
+    return count_chars
+#print(count_characters)
